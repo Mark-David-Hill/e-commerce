f5f76342cab07da2cd51b2d86ed0873c8ec7061e
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-04-16 15:09:09 -0600

    refactors ProductsList component out of Products page

---
+++ src/components/ProductsList.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect, useContext } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { CartContext } from "./context/CartProvider";
+
+import ProductCard from "./ProductCard";
+
+export default function ProductsList(props) {
+  const { cartItems } = useContext(CartContext);
+  const { categories, orderCategory, orderBy, searchTerm } = props;
+  const [numResults, setNumResults] = useState(0);
+
+  const idDesc = (a, b) => a.id - b.id;
+  const idAsc = (a, b) => b.id - a.id;
+  const alphabeticalDesc = (a, b) => a.title.localeCompare(b.title);
+  const alphabeticalAsc = (a, b) => b.title.localeCompare(a.title);
+  const priceDesc = (a, b) => a.price - b.price;
+  const priceAsc = (a, b) => b.price - a.price;
+
+  const categoryFilterCriteria = (product) =>
+    (categories.includes("women's clothing") &&
+      product.category === "women's clothing") ||
+    (categories.includes("men's clothing") &&
+      product.category === "men's clothing") ||
+    (categories.includes("jewelery") && product.category === "jewelery") ||
+    (categories.includes("electronics") && product.category === "electronics");
+
+  const sortCriteria =
+    orderCategory === "id"
+      ? orderBy === "asc"
+        ? idAsc
+        : idDesc
+      : orderCategory === "alphabet"
+      ? orderBy === "asc"
+        ? alphabeticalAsc
+        : alphabeticalDesc
+      : orderCategory === "price"
+      ? orderBy === "asc"
+        ? priceAsc
+        : priceDesc
+      : undefined;
+
+  useEffect(() => {
+    setNumResults(0);
+    if (cartItems) {
+      cartItems.forEach((item) => {
+        searchTerm &&
+          (item.product.title.toLowerCase().includes(searchTerm) ||
+            item.product.description
+              .toLowerCase()
+              .includes(searchTerm.toLowerCase())) &&
+          setNumResults((prev) => prev + 1);
+      });
+    }
+  }, [searchTerm, cartItems, numResults]);
+
+  return (
+    <div className="products-list-wrapper">
+      {categories.length === 0 ? (
+        <h3>No results. Please select a category</h3>
+      ) : searchTerm.trim() !== "" && numResults === 0 ? (
+        <h3>No products match your search. Please try again.</h3>
+      ) : cartItems.length > 0 ? (
+        cartItems
+          .map((item) => item.product)
+          .filter(categoryFilterCriteria)
+          .sort(sortCriteria)
+          .map((product) => {
+            return (
+              <ProductCard
+                key={product.id}
+                product={product}
+                searchTerm={searchTerm}
+              />
+            );
+          })
+      ) : (
+        <FontAwesomeIcon icon="fa-circle-notch" spin size="xl" />
+      )}
+    </div>
+  );
+}
--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,45 +1,19 @@
-import { useState, useEffect, useContext } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState, useEffect } from "react";
 
-import CategoryFilter from "./CategoryFilter";
-import ProductCard from "../ProductCard";
+import ProductsList from "../ProductsList";
+import CategoryFilter from "../CategoryFilter";
 import Search from "../Search";
-import { CartContext } from "../context/CartProvider";
 
 export default function Products(props) {
-  const { cartItems } = useContext(CartContext);
   const [orderBy, setOrderBy] = useState("desc");
   const [orderCategory, setOrderCategory] = useState("id");
   const [searchTerm, setSearchTerm] = useState("");
-  const [numResults, setNumResults] = useState(0);
   const [categories, setCategories] = useState([
     "women's clothing",
     "men's clothing",
     "jewelery",
     "electronics",
   ]);
-
-  const idDesc = (a, b) => a.id - b.id;
-  const idAsc = (a, b) => b.id - a.id;
-  const alphabeticalDesc = (a, b) => a.title.localeCompare(b.title);
-  const alphabeticalAsc = (a, b) => b.title.localeCompare(a.title);
-  const priceDesc = (a, b) => a.price - b.price;
-  const priceAsc = (a, b) => b.price - a.price;
-
-  const sortCriteria =
-    orderCategory === "id"
-      ? orderBy === "asc"
-        ? idAsc
-        : idDesc
-      : orderCategory === "alphabet"
-      ? orderBy === "asc"
-        ? alphabeticalAsc
-        : alphabeticalDesc
-      : orderCategory === "price"
-      ? orderBy === "asc"
-        ? priceAsc
-        : priceDesc
-      : undefined;
 
   const updateCategories = (checkboxWrapper) => {
     const checkedCategories = [];
@@ -51,14 +25,6 @@ export default function Products(props) {
     }
     setCategories(checkedCategories);
   };
-
-  const categoryFilterCriteria = (product) =>
-    (categories.includes("women's clothing") &&
-      product.category === "women's clothing") ||
-    (categories.includes("men's clothing") &&
-      product.category === "men's clothing") ||
-    (categories.includes("jewelery") && product.category === "jewelery") ||
-    (categories.includes("electronics") && product.category === "electronics");
 
   useEffect(() => {
     const checkboxWrapper =
@@ -93,20 +59,6 @@ export default function Products(props) {
     checkboxWrapper && updateCategories(checkboxWrapper);
   }, [props?.location?.state?.categories]);
 
-  useEffect(() => {
-    setNumResults(0);
-    if (cartItems) {
-      cartItems.forEach((item) => {
-        searchTerm &&
-          (item.product.title.toLowerCase().includes(searchTerm) ||
-            item.product.description
-              .toLowerCase()
-              .includes(searchTerm.toLowerCase())) &&
-          setNumResults((prev) => prev + 1);
-      });
-    }
-  }, [searchTerm, cartItems, numResults]);
-
   return (
     <div className="products-container">
       <h1>Products</h1>
@@ -117,28 +69,12 @@ export default function Products(props) {
         orderBy={orderBy}
       />
       <CategoryFilter updateCategories={updateCategories} />
-
-      {categories.length === 0 ? (
-        <h3>No results. Please select a category</h3>
-      ) : searchTerm.trim() !== "" && numResults === 0 ? (
-        <h3>No products match your search. Please try again.</h3>
-      ) : cartItems.length > 0 ? (
-        cartItems
-          .map((item) => item.product)
-          .filter(categoryFilterCriteria)
-          .sort(sortCriteria)
-          .map((product) => {
-            return (
-              <ProductCard
-                key={product.id}
-                product={product}
-                searchTerm={searchTerm}
-              />
-            );
-          })
-      ) : (
-        <FontAwesomeIcon icon="fa-circle-notch" spin size="xl" />
-      )}
+      <ProductsList
+        categories={categories}
+        orderCategory={orderCategory}
+        orderBy={orderBy}
+        searchTerm={searchTerm}
+      />
     </div>
   );
 }
