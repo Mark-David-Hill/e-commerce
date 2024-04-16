153bc614b87ae3b0f0635e6f999cd342ae1a506b
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-04-16 16:27:50 -0600

    adjust styling for home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,7 +3,10 @@ import Category from "../Category";
 export default function Home() {
   return (
     <div className="home-container">
-      <div className="hero-wrapper">
+      <div className="hero-section">
+        <h1>Akamarak Shopping</h1>
+      </div>
+      <div className="categories-wrapper">
         <Category categoryName={"men's clothing"} />
         <Category categoryName={"jewelery"} />
         <Category categoryName={"electronics"} />
