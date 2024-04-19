f425def781d5654e2eed45c861626e845bffaf28
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-04-19 15:35:22 -0600

    makes further adjustments to products page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -14,18 +14,18 @@ export default function Search(props) {
   return (
     <div className="search-wrapper">
       <div className="filter-wrapper">
-        <button onClick={() => setOrderBy(orderBy === "desc" ? "asc" : "desc")}>
-          {orderBy === "desc" ? (
-            <FontAwesomeIcon icon="fa-sort-up" />
-          ) : (
-            <FontAwesomeIcon icon="fa-sort-down" />
-          )}
-        </button>
         <select name="filter-by" id="filter-by" onChange={handleSelect}>
           <option value="id">Sort By ID:</option>
           <option value="price">By Price</option>
           <option value="alphabet">Alphabetical</option>
         </select>
+        <button onClick={() => setOrderBy(orderBy === "desc" ? "asc" : "desc")}>
+          {orderBy === "desc" ? (
+            <FontAwesomeIcon icon="fa-sort-down" />
+          ) : (
+            <FontAwesomeIcon icon="fa-sort-up" />
+          )}
+        </button>
       </div>
       <input
         type="text"
