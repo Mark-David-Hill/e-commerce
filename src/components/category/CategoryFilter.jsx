fdb7471fa2d4c86f7cf00fc21a946ad7623e9eee
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-04-24 15:27:49 -0600

    removes dom manipulation, removes categories from products page

--- src/components/category/CategoryFilter.jsx
+++ src/components/category/CategoryFilter.jsx
@@ -1,47 +1,54 @@
 export default function CategoryFilter(props) {
-  const { updateCategories } = props;
+  const { categories, setCategories } = props;
+
+  const updateCategories = (selectedCategory) => {
+    if (categories.includes(selectedCategory)) {
+      setCategories((prevCategories) =>
+        prevCategories.filter((category) => category !== selectedCategory)
+      );
+    } else {
+      setCategories((prevCategories) => [...prevCategories, selectedCategory]);
+    }
+  };
 
   return (
     <div className="category-filter">
       <p>Categories:</p>
-      <div
-        className="checkbox-wrapper"
-        onClick={() =>
-          updateCategories(
-            document.getElementsByClassName("checkbox-wrapper")[0]
-          )
-        }
-      >
+      <div className="checkbox-wrapper">
         <input
-          defaultChecked="true"
           type="checkbox"
           id="category1"
           name="category1"
           value="men's clothing"
+          checked={categories.includes("men's clothing")}
+          onChange={() => updateCategories("men's clothing")}
         />
         <label htmlFor="category1"> Men's Clothing</label>
         <input
-          defaultChecked="true"
           type="checkbox"
           id="category2"
           name="category2"
           value="jewelery"
+          checked={categories.includes("jewelery")}
+          onChange={() => updateCategories("jewelery")}
         />
         <label htmlFor="category2"> Jewelry</label>
         <input
-          defaultChecked="true"
           type="checkbox"
           id="category3"
           name="category3"
           value="electronics"
+          checked={categories.includes("electronics")}
+          onChange={() => updateCategories("electronics")}
         />
         <label htmlFor="category3"> Electronics</label>
         <input
-          defaultChecked="true"
           type="checkbox"
           id="category4"
           name="category4"
           value="women's clothing"
+          checked={categories.includes("women's clothing")}
+          onChange={() => updateCategories("women's clothing")}
         />
         <label htmlFor="category4"> Women's Clothing</label>
       </div>
--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -9,65 +9,18 @@ export default function Products(props) {
   const [orderCategory, setOrderCategory] = useState("id");
   const [searchTerm, setSearchTerm] = useState("");
   const [categories, setCategories] = useState([]);
-  //   "women's clothing",
-  //   "men's clothing",
-  //   "jewelry",
-  //   "electronics",
-  // ]]);
-
-  const updateCategories = (checkboxWrapper) => {
-    const checkedCategories = [];
-    for (let i = 0; i < checkboxWrapper.children.length; i++) {
-      const element = checkboxWrapper.children[i];
-      if (element.type === "checkbox" && element.checked) {
-        checkedCategories.push(element.value);
-      }
-    }
-    setCategories(checkedCategories);
-  };
 
   useEffect(() => {
-    if (categories && !props?.location?.state?.categories) {
+    if (!props?.location?.state?.categories) {
       fetch("https://fakestoreapi.com/products/categories")
         .then((res) => res.json())
         .then((json) => {
-          console.log(json);
           setCategories(json);
         });
-    }
-  }, []);
-
-  useEffect(() => {
-    const checkboxWrapper =
-      document.getElementsByClassName("checkbox-wrapper")[0];
-    if (props?.location?.state?.categories) {
+    } else {
       const chosenCategory = props.location.state.categories[0];
-      const checkboxes = [
-        document.getElementsByName("category1")[0],
-        document.getElementsByName("category2")[0],
-        document.getElementsByName("category3")[0],
-        document.getElementsByName("category4")[0],
-      ];
-      const setCheckboxes = (checkedId) => {
-        for (let i = 0; i < checkboxes.length; i++) {
-          checkedId === i + 1
-            ? (checkboxes[i].checked = true)
-            : (checkboxes[i].checked = false);
-        }
-      };
-      setCategories(chosenCategory);
-      if (chosenCategory === "men's clothing") {
-        setCheckboxes(1);
-      } else if (chosenCategory === "jewelery") {
-        setCheckboxes(2);
-      } else if (chosenCategory === "electronics") {
-        setCheckboxes(3);
-      } else if (chosenCategory === "women's clothing") {
-        setCheckboxes(4);
-      }
-      updateCategories(checkboxWrapper);
+      setCategories([chosenCategory]);
     }
-    checkboxWrapper && updateCategories(checkboxWrapper);
   }, [props?.location?.state?.categories]);
 
   return (
@@ -80,7 +33,7 @@ export default function Products(props) {
           setOrderBy={setOrderBy}
           orderBy={orderBy}
         />
-        <CategoryFilter updateCategories={updateCategories} />
+        <CategoryFilter categories={categories} setCategories={setCategories} />
       </div>
       <ProductsList
         categories={categories}
