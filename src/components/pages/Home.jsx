b267d81b20041420c00db085ca08227047a54e91
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-04-24 17:49:50 -0600

    fixes styling issues on home page and sets up loading icon

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,20 +2,26 @@ import { useContext } from "react";
 
 import Category from "../category/Category";
 import { CartContext } from "../context/CartProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Home() {
   const { categories } = useContext(CartContext);
 
   return (
     <div className="home-container">
-      <div className="hero-section">
-        <h1>Akamarak Shopping</h1>
-      </div>
-      <div className="categories-wrapper">
-        {categories &&
-          categories.map((category) => (
-            <Category key={category.id} categoryName={category.name} />
-          ))}
+      <div>
+        <div className="hero-section">
+          <h1>Akamarak Shopping</h1>
+        </div>
+        <div className="categories-wrapper">
+          {categories.length > 0 ? (
+            categories.map((category) => (
+              <Category key={category.id} categoryName={category.name} />
+            ))
+          ) : (
+            <FontAwesomeIcon icon="fa-circle-notch" spin size="xl" />
+          )}
+        </div>
       </div>
     </div>
   );
